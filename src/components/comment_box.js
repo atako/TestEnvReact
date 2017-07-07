9893019c2fa64f496992e6dc274bf9397f0b12b2
Dmitry Ryumkin <riumkin@gmail.com>
2017-07-07 11:11:58 +0300

    Add test for submit

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 
 export default class CommentBox extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { comment: ''};
+  }
+
+  onHandleChange(event) {
+    this.setState({ comment: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({ comment: '' });
+  }
+
   render() {
     return(
-      <div className='comment-box'>
-        <textarea />
-        <button>Submit button</button>
-      </div>
+      <form onSubmit={this.handleSubmit.bind(this)} className='comment-box'>
+        <textarea 
+          value={this.state.comment}
+          onChange={this.onHandleChange.bind(this)}/>
+        <button action="submit">Submit button</button>
+      </form>
     );
   }
 }
